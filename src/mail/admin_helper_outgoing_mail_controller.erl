bdd55289a19aa8ba463e99035584c1cce25a3374
Gustavo León Tramontin <gust989@gmail.com>
2014-10-16 10:03:59 -0430

    Update and rename mega_billing_outgoing_mail_controller.erl to admin_helper_outgoing_mail_controller.erl

--- src/mail/admin_helper_outgoing_mail_controller.erl
+++ src/mail/admin_helper_outgoing_mail_controller.erl
@@ -1,4 +1,4 @@
--module(mega_billing_outgoing_mail_controller).
+-module(admin_helper_outgoing_mail_controller).
 -compile(export_all).
 
 %% See http://www.chicagoboss.org/api-mail-controller.html for what should go in here
